7e070ba6843e2535a42209b72ea6e8581c8cd15e
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-11 16:04:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_english_meaning(file_path, emoticon)
   emojis = load_library(file_path)
   #binding.pry
   emojis.each do |emoji_name, translations|
-    #binding.pry
+    binding.pry
     if translations.any? { |jp_emoji| jp_emoji == emoticon }
       return emoji_name
     end
