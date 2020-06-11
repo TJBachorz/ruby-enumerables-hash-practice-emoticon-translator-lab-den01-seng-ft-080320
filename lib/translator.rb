589258388eae4362ba2248b25917ec5e1d28c16f
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-11 15:55:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_english_meaning(file_path, emoticon)
   binding.pry
   emojis.each do |emoji, translations|
     if emojis[emoji].any? { |jp_emoji| jp_emoji } == emoticon
-      return emojis[emoji][:english]
+      return emoji
     end
   end
 end
