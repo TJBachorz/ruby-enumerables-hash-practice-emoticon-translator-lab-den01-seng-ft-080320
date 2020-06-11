257433b59e7681a1c480f4a4b913560fba4d28f5
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-11 15:39:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,11 +5,11 @@ require "yaml"
 
 def load_library(file_path)
   emoji_list = YAML.load_file(file_path)
-  binding.pry
   emojis = Hash.new
   emoji_list.each do |k, v|
     emojis[k] = {english: v[0], japanese: v[1]}
   end
+  binding.pry
   emojis
 end
 
