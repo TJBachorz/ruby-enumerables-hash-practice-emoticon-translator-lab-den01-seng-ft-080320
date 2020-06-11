c5e07742ecd7bf1a41ae06fcdff3ffd2fc48a33a
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-11 15:34:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,9 @@ require "yaml"
 
 
 def load_library(file_path)
-  emojis = YAML.load_file(file_path)
+  emoji_list = YAML.load_file(file_path)
   binding.pry
-  emojis
+  emoji_list
 end
 
 def get_japanese_emoticon
